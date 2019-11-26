2b52afc061f218e2d8f92651bbc970ff44f8d0bc
Maria Malkova <marimalkova98@gmail.com>
2019-11-26 21:00:07 +0300

    added Rabbit

---
+++ src/main/java/com/itmolab/warehouse/configuration/RabbitConfiguration.java
@@ -0,0 +1,28 @@
+package com.itmolab.warehouse.configuration;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfiguration {
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange("directExchange");
+    }
+
+    @Bean
+    public Queue queue() {
+        return  new Queue("queue");
+    }
+
+    @Bean
+    public Binding binding(DirectExchange directExchange, Queue queue) {
+        return BindingBuilder.bind(queue).to(directExchange).with("item");
+    }
+
+}
--- src/main/java/com/itmolab/warehouse/controller/WarehouseController.java
+++ src/main/java/com/itmolab/warehouse/controller/WarehouseController.java
@@ -1,15 +1,21 @@
 package com.itmolab.warehouse.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itmolab.warehouse.model.Item;
 import com.itmolab.warehouse.model.Message;
 import com.itmolab.warehouse.service.IItemService;
 import io.swagger.annotations.Api;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 
+@EnableRabbit
 @RestController
 @RequestMapping("/api/warehouse")
 public class WarehouseController {
@@ -32,13 +38,13 @@ public class WarehouseController {
     @GetMapping("/items/{item_id}")
     public Item findItemById(@PathVariable(value = "item_id") Long  id) {
         return itemService.findById(id).get();
-
     }
+
+
 
     @PostMapping ("/items")
     @ResponseBody
     public Message createItem(@Valid @RequestBody Item item ) {
-        //System.out.println(item);
         itemService.CreateItem(item.getName(), item.getAmount(),item.getPrice());
         var message = new Message("Item is succesfully created!", 200);
         return message;
@@ -47,9 +53,25 @@ public class WarehouseController {
 
     @PutMapping("/items/{item_id}/addition/{amount}")
     public Message increaseItemAmount(@PathVariable(value = "item_id") Long id, @PathVariable(value = "amount") Integer amount) {
-
+        //var message = new Message("", 0);
         itemService.IncreaseItemAmount(id, amount);
         var message = new Message("Updated succesfully!", 200);
+//        Item item = itemService.findById(id).get();
+//        if ((item.getAmount()+amount) >= 0) {
+//            itemService.IncreaseItemAmount(id, amount);
+//            message = new Message("Updated succesfully!", 200);
+//        } else {message = new Message("Not enought items", 410);}
+
         return message;
+    }
+
+    @RabbitListener(queues = "queue")
+    public void updateItemAmount(byte[] message) throws IOException {
+        String mess = new String(message);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode actualObj = mapper.readTree(mess);
+        Long id = actualObj.get("item_id").asLong();
+        Integer amount = actualObj.get("amount").asInt();
+        itemService.IncreaseItemAmount(id, amount);
     }
 }
